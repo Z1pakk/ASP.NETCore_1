5abf8cf6eea39844317d54c5d4afa81e54b07a3a
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-05 14:03:53 +0300

    Simple ViewBag

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -17,6 +17,8 @@ namespace PartyInvites.Controllers
 
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
     }
