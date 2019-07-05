1d3272a80469a66b921532e17d531160a48f32d6
Vlad Shusmkiy <wladik420@gmail.com>
2019-07-05 13:50:30 +0300

    First

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "Hello world";
+        //}
+
+        public 
+    }
+}
